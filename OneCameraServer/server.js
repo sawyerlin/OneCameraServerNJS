962cbd611a00632e0136dce9414509634763f946
sawyerlin <sawyer.lin@gmail.com>
2014-10-26 22:02:38 +0100

    require underscore, update server

--- OneCameraServer/server.js
+++ OneCameraServer/server.js
@@ -1,5 +1,6 @@
 var WebSocketServer = require('websocket').server;
 var http = require('http');
+var _ = require('underscore');
 
 var server = http.createServer(function(request, response){
 	console.log((new Date()) + ' Received request for ' + request.url);
@@ -11,14 +12,23 @@ server.listen(8080, function() {
 	console.log((new Date()) + 'Server is listening on port 8080');
 });
 
-wsServer = new WebSocketServer({
-	httpServer: server,
-	autoAcceptConnections: false
 
+var connections = [];
+
+var wsServer = new WebSocketServer({
+	httpServer: server,
+	autoAcceptConnections: false,
+	maxReceivedFrameSize: '5MiB',
+	maxReceivedMessageSize: '50MiB'
 });
 
 function originIsAllowed(origin){
 	return true;
+}
+
+function broadCast(data){
+	var receivers = _.where(connections, {resource: '/receive'});
+	console.log(receivers.length);
 }
 
 wsServer.on('request', function(request){
@@ -29,22 +39,28 @@ wsServer.on('request', function(request){
 	}
 
 	var connection = request.accept('echo-portocol', request.origin);
+	connection.resource = request.resource;
+	console.log(connection.resource);
+	connections.push(connection);
+	_.each(connections, function(con){console.log(con.remoteAddress + " Connected: " + con.connected);});
 	console.log((new Date()) + ' Connection accepted.');
 	connection.on('message', function(message){
-		console.log(message)
+		broadCast(message);
 		if(message.type === 'utf8'){
-			console.log('Received Message: ' + message.utf8Data);
-			connection.sendUTF(message.utf8Data);
+		//	console.log('Received Message: ' + message.utf8Data);
+		//	connection.sendUTF(message.utf8Data);
 		}	
 		else if (message.type === 'binary') {
-			console.log('Received Binary Message of ' + message.binaryData.length + ' bytes')
-			connection.sendBytes(message.binaryData);
+		//	console.log('Received Binary Message of ' + message.binaryData.length + ' bytes')
+		//	connection.sendBytes(message.binaryData);
+		}
+		else {
+			// console.log(message)
 		}
 	});
-	
-	connection.on('close', function(reasonCode, description) {
-		console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
 
+	connection.on('close', function(reasonCode, description) {
+		connections.splice(connections.indexOf(this), 1);
+		console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected. ' + 'Reason: ' + reasonCode + '. Description ' + description);
 	});
 });
-
